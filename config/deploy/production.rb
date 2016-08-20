844a8d8260174e0a3e98f2afaca60d72acca3572
Omair <omair@gems.techverx.com>
2016-08-21 02:58:33 +0500

    images added to public directory

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 
-server '52.207.235.29', user: 'deploy', roles: %w{web app db}
+server '52.6.141.29', user: 'deploy', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
