91954c2c4921aadd19d5ea0eb5685076e4df1143
Omair <omair@gems.techverx.com>
2016-10-21 18:29:08 +0500

    paypal configuration. Things are fine. Ready for live paypal url

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -9,17 +9,17 @@ class Transaction < ActiveRecord::Base
 	        no_shipping: 1,
 	        upload: 1,
 	        return: "#{Rails.application.secrets.app_host}#{return_path}",
-	        invoice: 15000+id,
+	        invoice: id,
 	        item_name: user.email,
 	        notify_url: "#{Rails.application.secrets.app_host}/hook",  
 	        cmd: "_xclick-subscriptions",
             a3: package.amount,
-            p3: 1,
+            p3: 1,  # repeat every t3 value e.g for M, 1 means repeat every month
             src: 1, # recurring payments, 1 for true
-            srt: package.cycles, #Recurring times. 
-            #srt: 2,
-            #t3: user.period.first,
-            t3: 'D' # monthly or daily
+            #srt: package.cycles, #Recurring times. 
+            srt: 52,
+            t3: user.period.first,
+            #t3: 'D' # monthly or daily
             #rm:2
 	    }
 
