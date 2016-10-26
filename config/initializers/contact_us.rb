e4e84a9fa8dfce9f4d68138994d4624c0e49f0fc
Omair <omair@gems.techverx.com>
2016-10-26 19:33:19 +0500

    contact us mailer email setup

--- config/initializers/contact_us.rb
+++ config/initializers/contact_us.rb
@@ -6,7 +6,7 @@ ContactUs.setup do |config|
   # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
   # Example:
   # config.mailer_from = "contact@please-change-me.com"
-  config.mailer_from = "dontreply@yourdomain.com"
+  config.mailer_from = "admin@indextresearch.com"
 
   # Configure the e-mail address which should receive the contact form email notifications.
   config.mailer_to = "info@indextresearch.com"
